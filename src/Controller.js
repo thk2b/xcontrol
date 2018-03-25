c3217cf705656df0149467bc2bde2af6d43ce670
Theo <theokobb@gmail.com>
2018-03-25 18:20:22 -0400

    implement Controller

---
+++ src/Controller.js
@@ -0,0 +1,19 @@
+export default class Controller {
+    constructor(initialState){
+        this._subscribers = []
+        this._state = initialState
+    }
+    subscribe(notifyCb){
+        this._subscribers.push(notifyCb)
+        // TODO: return an unsubscribe function
+    }
+    get state(){
+        return this._state
+    }
+    set state(newState){
+        this._subscribers.forEach(
+            subscriber => subscriber(newState, this._state)
+        )
+        this._state = newState
+    }
+}
---
+++ src/test/Controller.test.js
@@ -0,0 +1,34 @@
+import test from 'tape'
+import { Controller } from '../'
+
+test('constructor', t => {
+    const c = new Controller('test')
+    t.ok(typeof c.subscribe === 'function')
+    t.end()
+})
+
+test('subscribing to state updates', t => {
+    const _ = new Controller()
+    const c = new Controller()
+    const value = 'test'
+
+    _.subscribe(() => t.fail('should not notify other controllers'))
+    c.subscribe( state => {
+        t.ok(state === value)
+        t.end()
+    })
+
+    c.state = value
+})
+test('unsubscribing from state updates', t => {
+    t.end()
+})
+
+test('getting the state', t => {
+    const value = 'test'
+    const c = new Controller(value)
+
+    t.ok(c.state === 'test')
+
+    t.end()
+})
