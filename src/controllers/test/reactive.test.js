5c7ae3d81c216b90e8e72fc5cea1f9580563ce7a
Theo <theokobb@gmail.com>
2018-03-27 16:58:04 -0400

    refactor reactive tests

--- src/controllers/test/reactive.test.js
+++ src/controllers/test/reactive.test.js
@@ -3,87 +3,120 @@ import sinon from 'sinon'
 
 import reactive from '../reactive'
 import Model from '../../models/Model'
+import Value from '../../models/Value'
+import HashMap from '../../models/HashMap';
 
-class Controller extends reactive(Model){
+Object.entries({
+    'Model': reactive(Model),
+    'Value': reactive(Value),
+    'HashMap': reactive(HashMap)
+}).forEach(runTestSuite)
 
+function runTestSuite([name, ReactiveModel]){
+    /* This test suite ensures that all models can be made reactive.
+    ** This suite ignores actions specific to each model.
+    ** Instead, it ensures they all have basic reactive properties.
+    */
+    name = `reactive(${name})`
+    test(name, main => {
+        main.test('constructor', t => {
+            const assert = c => {
+                t.ok(typeof c.subscribe === 'function')
+            }
+            t.test('extending ' + name, t => {
+                const c = new ReactiveModel()
+                assert(c)
+                t.end()
+            }) 
+            t.test('new new ' + name , t => {
+                const c = new new reactive(Model)
+                /* caveat: doesn't new reactive... work ?*/
+                assert(c)
+                t.end()
+            }) 
+        })
+        main.test('subscribing to state updates', t => {
+            const initialState = 'initial state'
+            const c = new ReactiveModel(initialState)
+            const callback1 = sinon.spy()    
+            const callback2 = sinon.spy()    
+            c.subscribe(callback1)
+            c.subscribe(callback2)
+        
+            t.ok(callback1.calledOnceWith(initialState) &&
+                callback2.calledOnceWith(initialState),
+                'should immediately notify subscribers'
+            )
+        
+            const other = new ReactiveModel()
+            const innocentCallback = sinon.spy()
+            other.subscribe(innocentCallback)
+            
+            const value = 'test'
+        
+            c.state = value
+            t.ok(callback1.calledWith(value) && callback2.calledWith(value), 'should have notified subscribers with the new state')
+            t.ok(innocentCallback.callCount === 1, 'should not have notified other ReactiveModels')
+            t.end()
+        })
+        main.test('unsubscribing from state updates', t => {
+            /* In this test, we subscribe 5 times. 
+            ** The first 4 times, we catch the first state update.
+            ** We then unsubscribe 2 subscribers and subscribe a fith one.
+            ** We cause a final state update for the remaining 3 subscribers. 
+            */
+            const c = new ReactiveModel()
+            const value = 'test'
+            const newValue = 'new test'
+        
+            const callback = sinon.spy()
+            const callback1 = sinon.spy()
+            const callback2 = sinon.spy()
+            const callback3 = sinon.spy()
+            const callback4 = sinon.spy()
+            
+            const unsubscribe = c.subscribe(callback)
+            t.ok(typeof unsubscribe === 'function', 'should return a function')
+            const unsubscribe1 = c.subscribe(callback1)
+            const unsubscribe2 = c.subscribe(callback2)
+            const unsubscribe3 = c.subscribe(callback3)
+            
+            c.state = value
+            
+            const didUnsubscribe = unsubscribe()
+            t.ok(didUnsubscribe, 'should return true when the subscriber is removed')
+            unsubscribe2()
+        
+            const unsubscribe4 = c.subscribe(callback4)    
+        
+            c.state = newValue
+        
+            t.ok(callback.calledTwice)
+            t.ok(callback1.calledThrice)
+            t.ok(callback2.calledTwice)
+            t.ok(callback3.calledThrice)
+            t.ok(callback4.calledTwice)
+            t.end()
+        })
+    })
 }
 
-test('reactive(Model)', main => {
-    main.test('constructor', t => {
-        const c = new Controller()
-        t.ok(typeof c.subscribe === 'function')
-        t.end()
-    })
-    main.test('subscribing to state updates', t => {
-        const initialState = 'initial state'
-        const c = new Controller(initialState)
-        const callback1 = sinon.spy()    
-        c.subscribe(callback1)
-        const callback2 = sinon.spy()    
-        c.subscribe(callback2)
-    
-        t.ok(callback1.calledOnceWith(initialState) &&
-            callback2.calledOnceWith(initialState),
-            'should immediately notify subscribers'
-        )
-    
-        const other = new Controller()
-        const innocentCallback = sinon.spy()
-        other.subscribe(innocentCallback)
-        
-        const value = 'test'
-    
-        c.state = value
-        t.ok(callback1.calledWith(value) && callback2.calledWith(value), 'should have notified subscribers with the new state')
-        t.ok(innocentCallback.callCount === 1, 'should not have notified other controllers')
-        t.end()
-    })
-    main.test('unsubscribing from state updates', t => {
-        /* In this test, we subscribe 5 times. 
-        ** The first 4 times, we catch the first state update.
-        ** We then unsubscribe 2 subscribers and subscribe a fith one.
-        ** We cause a final state update for the remaining 3 subscribers. 
-        */
-        const c = new Controller()
-        const value = 'test'
-        const newValue = 'new test'
-    
-        const callback = sinon.spy()
-        const callback1 = sinon.spy()
-        const callback2 = sinon.spy()
-        const callback3 = sinon.spy()
-        const callback4 = sinon.spy()
-        
-        const unsubscribe = c.subscribe(callback)
-        t.ok(typeof unsubscribe === 'function', 'should return a function')
-        const unsubscribe1 = c.subscribe(callback1)
-        const unsubscribe2 = c.subscribe(callback2)
-        const unsubscribe3 = c.subscribe(callback3)
-        
-        c.state = value
-        
-        const didUnsubscribe = unsubscribe()
-        t.ok(didUnsubscribe, 'should return true when the subscriber is removed')
-        unsubscribe2()
-    
-        const unsubscribe4 = c.subscribe(callback4)    
-    
-        c.state = newValue
-    
-        t.ok(callback.calledTwice)
-        t.ok(callback1.calledThrice)
-        t.ok(callback2.calledTwice)
-        t.ok(callback3.calledThrice)
-        t.ok(callback4.calledTwice)
-        t.end()
-    })
-    main.test('extending Controller with custom actions', t => {
-        class Value extends Controller {
-            set(val){
-                this.state = val
-            }
-        }
-        const v = new Value('initial')
+class ReactiveModel extends reactive(HashMap){}
+class ReactiveValue extends reactive(Value){}
+class AsyncReactiveValue extends ReactiveValue {
+    set(value){
+        setTimeout(() => super.set(value), 0)
+    }
+}
+
+test('advanced Model reactivity', main => {
+    /** This test suite guarantees that Models with custom actions
+     ** are still reactive.
+     ** These actions can be pure, async or cause actions in other models
+     ** In sum, this suite guarantees that reactivity is preserved on any compliant Model.
+     */
+    main.test('reactive Model with custom actions', t => {
+        const v = new ReactiveValue('initial')
     
         const cb = sinon.spy()
         v.subscribe(cb)
@@ -94,8 +127,8 @@ test('reactive(Model)', main => {
         t.ok(cb.calledWith(newValue))
         t.end()
     })
-    main.test('extending Controller with custom selectors', t => {
-        class Counter extends Controller {
+    main.test('reactive Model with custom selectors', t => {
+        class Counter extends reactive(Model) {
             increment(by = 1){
                 this.state = this.state + by
             }
@@ -115,13 +148,9 @@ test('reactive(Model)', main => {
         t.ok(counter.isGreaterThan(1), 'selector should be true')
         t.end()
     })
-    main.test('extending Controller with async actions', t => {
-        class Value extends Controller {
-            set(val){
-                setTimeout(() => this.state = val, 0)
-            }
-        }
-        const v = new Value('initial')
+    main.test('extending reactive Models with async actions', t => {
+        const initialState = 'initial state'
+        const v = new AsyncReactiveValue(initialState)
     
         const cb = sinon.spy()
         v.subscribe(cb)
@@ -135,12 +164,7 @@ test('reactive(Model)', main => {
         }, 1)
     })
     main.test('causing actions asynchronously', t => {
-        class Value extends Controller {
-            set(val){
-                this.state = val
-            }
-        }
-        const v = new Value()
+        const v = new ReactiveValue()
         const callback = sinon.spy()
         v.subscribe(callback)
     
@@ -154,17 +178,12 @@ test('reactive(Model)', main => {
         }, 1)
     
     })
-    main.test('calling another controller\'s actions', t => {
-        class Value extends Controller {
-            set(val){
-                this.state = val
-            }
-        }
-        const value = new Value()
+    main.test('calling another reactive model\'s actions', t => {
+        const value = new ReactiveValue()
         const valueCb = sinon.spy()
         value.subscribe(valueCb)
     
-        class UpperCaseValue extends Controller {
+        class UpperCaseValue extends ReactiveValue {
             setToUpperCaseAndHaveSideEffects(val){
                 value.set(val)
                 this.state = val.toUpperCase()
@@ -180,5 +199,23 @@ test('reactive(Model)', main => {
         t.ok(upperValueCb.calledWith(testValue.toUpperCase()), 'should notify subscriber with own state')
         t.ok(valueCb.calledWith(testValue), 'should have notified the other controller\'s subscribers')
         t.end()
+    })
+    main.test('subscribing to another reactive model', t => {
+        class A extends ReactiveValue { }
+        class B extends ReactiveValue { }
+
+        const initialState = 'initial state'
+        const a = new A(initialState)
+        const b = new B()
+
+        a.subscribe( aState => b.set(aState))
+        t.equal(a.state, initialState, 'should have causes an initial state update')
+        
+        const nextState = 'next state'
+        setTimeout(() => a.set(nextState), 0)
+        setTimeout(() => {
+            t.end()
+            t.equal(b.state, nextState)
+        }, 1)
     })
 })
