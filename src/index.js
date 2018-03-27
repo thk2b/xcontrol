75fd187919a7a5ae693d27cc91515e6b69966a21
Theo <theokobb@gmail.com>
2018-03-27 17:22:05 -0400

    public exports

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as reactive } from './controllers/reactive'
+export { default as computed } from './controllers/computed'
+export { default as subscribed } from './controllers/subscribed'
